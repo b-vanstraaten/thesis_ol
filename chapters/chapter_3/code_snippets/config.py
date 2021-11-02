47da32e0283b92cf4a98a7244ccab96532e68d2e
b-vanstraaten <barnaby.vanstraaten@materials.ox.ac.uk>
2021-11-02 18:46:55 +0000

    first commit

---
+++ chapters/chapter_3/code_snippets/config.py
@@ -0,0 +1,9 @@
+default_config = {
+    "version": 1.2,
+    "controllers": controllers,
+    "elements": elements,
+    "pulses": pulses,
+    "waveforms": waveforms,
+    "digital_waveforms": digital_waveforms,
+    "integration_weights": integration_weights,
+}
